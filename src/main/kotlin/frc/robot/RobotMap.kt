453f64d53cb5e0241793f8b64a7a755747a7cae6
Alon <alon.gal.kufsa@gmail.com>
2024-11-27 19:03:20 +0200

    Changed can_coder to cancoder

--- src/main/kotlin/frc/robot/RobotMap.kt
+++ src/main/kotlin/frc/robot/RobotMap.kt
@@ -5,25 +5,25 @@ object RobotMap {
 		object FrontLeft {
 			const val DRIVE_MOTOR_ID = 15
 			const val STEER_MOTOR_ID = 5
-			const val CAN_CODER_ID = 6
+			const val CANCODER_ID = 6
 		}
 
 		object FrontRight {
 			const val DRIVE_MOTOR_ID = 16
 			const val STEER_MOTOR_ID = 7
-			const val CAN_CODER_ID = 8
+			const val CANCODER_ID = 8
 		}
 
 		object BackLeft {
 			const val DRIVE_MOTOR_ID = 17
 			const val STEER_MOTOR_ID = 9
-			const val CAN_CODER_ID = 10
+			const val CANCODER_ID = 10
 		}
 
 		object BackRight {
 			const val DRIVE_MOTOR_ID = 18
 			const val STEER_MOTOR_ID = 11
-			const val CAN_CODER_ID = 12
+			const val CANCODER_ID = 12
 		}
 
 		const val PIGEON_2_ID = 2
